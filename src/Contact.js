df0ec17b2d34d07cbeec68e11411d605fdc3ca13
Dominika Kaźmierczak <dkl.kazmierczak@gmail.com>
2022-02-01 12:19:56 +0100

    Fixed className

--- src/Contact.js
+++ src/Contact.js
@@ -3,10 +3,10 @@ import "./App.css"
 const Contact = () => {
   return (
     <div className="Contact">
-      <div class="container container-contact">
-        <div class="contact" id="contact">
+      <div className="container container-contact">
+        <div className="contact" id="contact">
           Let's work together!
-          <div class="contact-email">
+          <div className="contact-email">
             Contact me by email{" "}
             <a href="mailto:dkl.kazmierczak@gmail.com" title="Email Dominika">
               dkl.kazmierczak@gmail.com
