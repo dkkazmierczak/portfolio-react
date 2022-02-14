05a0946e40bd9622fe95c5472874cd43bd27a84a
Dominika Kaźmierczak <dkl.kazmierczak@gmail.com>
2022-02-14 13:12:33 +0100

    updated site with html5

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -3,30 +3,28 @@ import "./Footer.scss"
 
 const Footer = () => {
   return (
-    <div className="Footer">
+    <footer className="Footer">
       <Container>
-        <footer>
-          <div className="contact-links">
-            <a href="mailto:dkl.kazmierczak@gmail.com">
-              <i className="fas fa-envelope" title="Email Dominika"></i>
-            </a>
-            <a
-              href="https://github.com/dkkazmierczak"
-              target="_blank"
-              rel="noreferrer">
-              <i className="fab fa-github-square" title="GitHub Profile"></i>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/dkkazmierczak/"
-              target="_blank"
-              title="LinkedIn Profile"
-              rel="noreferrer">
-              <i className="fab fa-linkedin"></i>
-            </a>
-          </div>
-        </footer>
+        <div className="contact-links">
+          <a href="mailto:dkl.kazmierczak@gmail.com">
+            <i className="fas fa-envelope" title="Email Dominika"></i>
+          </a>
+          <a
+            href="https://github.com/dkkazmierczak"
+            target="_blank"
+            rel="noreferrer">
+            <i className="fab fa-github-square" title="GitHub Profile"></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/dkkazmierczak/"
+            target="_blank"
+            title="LinkedIn Profile"
+            rel="noreferrer">
+            <i className="fab fa-linkedin"></i>
+          </a>
+        </div>
       </Container>
-    </div>
+    </footer>
   )
 }
 
--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -5,23 +5,21 @@ import "./Hero.scss"
 const Hero = () => {
   return (
     <div className="Hero">
-      <div className="hero">
-        <div className="row">
-          <div className="col-md-6 text">
-            <h2>Hi, I'm Dominika</h2>
-            <h1>Front-End Developer</h1>
-            <h3>Based in Halden, Norway</h3>
-            <a href="/index.html#about" className="big">
-              Learn more about me
-            </a>
-          </div>
-          <div className="col-md-6">
-            <img src={profilePicture} alt="Profile" />
-          </div>
-          <a href="/index.html#about" className="small">
+      <div className="row">
+        <header className="col-md-6 text">
+          <h2>Hi, I'm Dominika</h2>
+          <h1>Front-End Developer</h1>
+          <h3>Based in Halden, Norway</h3>
+          <a href="/index.html#about" className="big">
             Learn more about me
           </a>
+        </header>
+        <div className="col-md-6">
+          <img src={profilePicture} alt="Profile" />
         </div>
+        <a href="/index.html#about" className="small">
+          Learn more about me
+        </a>
       </div>
     </div>
   )
